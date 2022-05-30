ec38c9160efed3e94c9dee45365237cd4c9567ab
Peter Tran <pettran866@gmail.com>
2022-05-29 18:28:58 -0700

    added delete note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,23 +2,49 @@ const path = require("path");
 const router = require("express").Router();
 const fs = require('fs');
 const uuid = require('uuid');
-const notes = require('../../db/db');
 
-router.get('/notes', (req,res) => res.json(notes));
+// function to get notes
+const getNotes = () => {
+  const notes = fs.readFileSync(path.join(__dirname,'../../db/db.json'), {'encoding': 'utf8'});
+  return JSON.parse(notes);
+}
 
-// Post Route to add a new note to the database
+// function to write to file
+const writeToFile = (fileName, data) => {
+  fs.writeFile(fileName, data, (err) => {
+    if(err) throw err;
+  })
+}
+
+// Get route to get the notes
+router.get('/notes', (req,res) => res.json(getNotes()));
+
+// Post route to add a new note to the database
 router.post('/notes', (req,res) => {
+
   // get new note and push to note array
   let newNote = req.body;
+  // add unique id
   newNote.id = uuid.v4();
+
+  // get notes database and add new note to it
+  const notes = getNotes();
   notes.push(newNote);
 
   // write to database file
-  fs.writeFile(path.join(__dirname,'../../db/db.json'), JSON.stringify(notes), err => {
-    if(err) throw err;
-    console.log('Note saved');
-  })
+  writeToFile(path.join(__dirname,'../../db/db.json'), JSON.stringify(notes));
   res.send("Note saved");
+})
+
+// Delete route to delete a specifc note
+router.delete(`/notes/:id`, (req,res) =>  {
+  // get notes from database
+  const notes = getNotes();
+  // filter the notes to remove note with the specify id
+  newNotes = notes.filter(note => note.id != req.params.id);
+  // write to database file
+  writeToFile(path.join(__dirname,'../../db/db.json'), JSON.stringify(newNotes));
+  res.send("Note deleted");
 })
 
 module.exports = router;
